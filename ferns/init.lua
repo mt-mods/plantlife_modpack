b815ffc578dc817a16087434a48a97a4206879da
Craig Robbins <kde.psych@gmail.com>
2014-08-23 20:23:39 +1000

    Updated "lady ferns"

--- ferns/init.lua
+++ ferns/init.lua
@@ -7,8 +7,6 @@ local mname		= "ferns" -- former "archaeplantae"
 -- License (everything): 	WTFPL			
 -----------------------------------------------------------------------------------------------
 
-local run_tests = true
-
 abstract_ferns = {}
 
 dofile(minetest.get_modpath("ferns").."/settings.lua")
@@ -31,13 +29,26 @@ end
 
 dofile(minetest.get_modpath("ferns").."/crafting.lua")
 
+
+-----------------------------------------------------------------------------
+-- TESTS
+-----------------------------------------------------------------------------
+local run_tests = true	-- set to false to skip
+
 if run_tests then
 	-- Check node names
-	if abstract_ferns.config.enable_horsetails  then
+	if abstract_ferns.config.enable_horsetails then
+		print("[Mod] " ..title.. " Checking horsetail item strings")
 		assert(minetest.registered_items["ferns:horsetail_01"] ~= nil)
 		assert(minetest.registered_items["ferns:horsetail_02"] ~= nil)
 		assert(minetest.registered_items["ferns:horsetail_03"] ~= nil)
 		assert(minetest.registered_items["ferns:horsetail_04"] ~= nil)
+	end
+	if abstract_ferns.config.enable_lady_fern then
+		print("[Mod] ".. title .." Checking lady fern item strings")
+		assert(minetest.registered_items["ferns:fern_01"] ~= nil)
+		assert(minetest.registered_items["ferns:fern_02"] ~= nil)
+		assert(minetest.registered_items["ferns:fern_03"] ~= nil)
 	end
 end
 
