63914caed1bc8ad26909dc9095f8b3db945a909c
Jordan Leppert <jordanleppert@gmail.com>
2021-12-04 16:39:39 +0000

    Removing attached_node from vine groups, as it was causing breaking one vine to break the vine all the way up to the top, instantly

--- vines/init.lua
+++ vines/init.lua
@@ -56,7 +56,7 @@ end
 
 vines.register_vine = function( name, defs, biome )
 
-	local groups = {vines = 1, snappy = 3, flammable = 2, attached_node = 1}
+	local groups = {vines = 1, snappy = 3, flammable = 2}
 	local vine_name_end = 'vines:' .. name .. '_end'
 	local vine_name_middle = 'vines:' .. name .. '_middle'
 	local vine_image_end = "vines_" .. name .. "_end.png"
@@ -111,7 +111,6 @@ vines.register_vine = function( name, defs, biome )
 		on_construct = function(pos)
 
 			local timer = minetest.get_node_timer(pos)
-
 			timer:start(math.random(5, 10))
 		end,
 
@@ -120,7 +119,6 @@ vines.register_vine = function( name, defs, biome )
 			local node = minetest.get_node(pos)
 			local bottom = {x = pos.x, y = pos.y - 1, z = pos.z}
 			local bottom_node = minetest.get_node( bottom )
-
 			if bottom_node.name == "air" then
 
 				if not math.random(defs.average_length) == 1 then
