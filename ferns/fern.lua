f812ccb546970a30cb7b02794037c13e84eaed5a
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-05-25 22:48:22 -0400

    translate ferns.lua

--- ferns/fern.lua
+++ ferns/fern.lua
@@ -4,16 +4,12 @@
 -- by Mossmanikin
 -- Contains code from:		biome_lib
 -- Looked at code from:		default, flowers, painting, trees
--- Dependencies:			biome_lib
 -- Supports:				dryplants, stoneage, sumpf
 -----------------------------------------------------------------------------------------------
 -- some inspiration from here
 -- https://en.wikipedia.org/wiki/Athyrium_yokoscense
 -- http://www.mygarden.net.au/gardening/athyrium-yokoscense/3900/1
 -----------------------------------------------------------------------------------------------
-
--- support for i18n
-local S = minetest.get_translator("ferns")
 
 minetest.register_alias("ferns:fern_03", "default:fern_3")
 minetest.register_alias("ferns:fern_02", "default:fern_2")
@@ -39,8 +35,11 @@ end
 -- Spawning
 -----------------------------------------------------------------------------------------------
 
-biome_lib.register_on_generate({ -- near trees (woodlands)
-	surface = {
+minetest.register_decoration({ -- near trees (woodlands)
+	decoration = nodenames,
+	deco_type = "simple",
+	flags = "all_floors",
+	place_on = {
 		"default:dirt_with_grass",
 		"default:mossycobble",
 		"default:desert_sand",
@@ -49,25 +48,19 @@ biome_lib.register_on_generate({ -- near trees (woodlands)
 		"stoneage:grass_with_silex",
 		"sumpf:sumpf"
 	},
-	max_count = 30,
-	rarity = 62,--63,
-	min_elevation = 1, -- above sea level
-	near_nodes = {"group:tree"},
-	near_nodes_size = 3,--4,
-	near_nodes_vertical = 2,--3,
-	near_nodes_count = 1,
-	plantlife_limit = -0.9,
-	humidity_max = -1.0,
-	humidity_min = 0.4,
-	temp_max = -0.5, -- 55 °C (too hot?)
-	temp_min = 0.75, -- -12 °C
-	random_facedir = { 0, 179 },
-},
-nodenames
-)
+	y_min = 1, -- above sea level
+	param2 = 0,
+	param2_max = 179,
+	spawn_by = "group:tree",
+	num_spawn_by = 1,
+	fill_ratio = 0.1,
+})
 
-biome_lib.register_on_generate({ -- near stone (mountains)
-	surface = {
+minetest.register_decoration({ -- near stone (mountains)
+	decoration = nodenames,
+	deco_type = "simple",
+	flags = "all_floors",
+	place_on = {
 		"default:dirt_with_grass",
 		"default:mossycobble",
 		"group:falling_node",
@@ -75,56 +68,42 @@ biome_lib.register_on_generate({ -- near stone (mountains)
 		"stoneage:grass_with_silex",
 		"sumpf:sumpf"
 	},
-	max_count = 35,
-	rarity = 40,
-	min_elevation = 1, -- above sea level
-	near_nodes = {"group:stone"},
-	near_nodes_size = 1,
-	near_nodes_count = 16,
-	plantlife_limit = -0.9,
-	humidity_max = -1.0,
-	humidity_min = 0.4,
-	temp_max = -0.5, -- 55 °C (too hot?)
-	temp_min = 0.75, -- -12 °C
-	random_facedir = { 0, 179 },
-},
-nodenames
-)
+	y_min = 1, -- above sea level
+	param2 = 0,
+	param2_max = 179,
+	spawn_by = "group:stone",
+	num_spawn_by = 1,
+	fill_ratio = 0.3,
+})
 
-biome_lib.register_on_generate({ -- near ores (potential mining sites)
-	surface = {
+minetest.register_decoration({ -- near stone (mountains)
+	decoration = nodenames,
+	deco_type = "simple",
+	flags = "all_floors",
+	place_on = {
 		"default:dirt_with_grass",
 		"default:mossycobble",
 		"default:stone_with_coal",
 		"default:stone_with_iron",
-		"default:stone_with_tin", -- minetest >= 0.4.16
+		"default:stone_with_tin",
 		"moreores:mineral_tin",
 		"moreores:mineral_silver",
 		"sumpf:sumpf"
 	},
-	max_count = 1200,--1600, -- maybe too much? :D
-	rarity = 25,--15,
-	min_elevation = 1, -- above sea level
-	near_nodes = {
+	y_min = 1, -- above sea level
+	param2 = 0,
+	param2_max = 179,
+	spawn_by = {
 		"default:stone_with_iron",
 		--"default:stone_with_copper",
 		--"default:stone_with_mese",
 		--"default:stone_with_gold",
 		--"default:stone_with_diamond",
-		"default:stone_with_tin", -- minetest >= 0.4.16
+		"default:stone_with_tin",
 		"moreores:mineral_tin",
 		"moreores:mineral_silver"
 		--"moreores:mineral_mithril"
 	},
-	near_nodes_size = 2,
-	near_nodes_vertical = 4,--5,--6,
-	near_nodes_count = 2,--3,
-	plantlife_limit = -0.9,
-	humidity_max = -1.0,
-	humidity_min = 0.4,
-	temp_max = -0.5, -- 55 °C (too hot?)
-	temp_min = 0.75, -- -12 °C
-	random_facedir = { 0, 179 },
-},
-nodenames
-)
+	num_spawn_by = 1,
+	fill_ratio = 0.8,
+})
